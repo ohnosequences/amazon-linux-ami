27fec995f5ab77715f8d61f0deb1d15038b2d959
Alexey Alekhin <laughedelic@gmail.com>
2013-05-17 16:33:59 +0200

    Fixes after testing

--- src/main/scala/Ami44939930.scala
+++ src/main/scala/Ami44939930.scala
@@ -9,7 +9,7 @@ import ohnosequences.statika.AmiBundle
 object Ami44939930_2013_03 extends AmiBundle(
     id = "ami-44939930",
     amiVersion = "2013.03",
-    version = "0.1.0-SNAPSHOT"
+    version = "0.1.0"
   ) {
 
   def userScript[B <: BundleAux](b: B, artifact: String)
@@ -21,41 +21,52 @@ exec &> /root/log.txt
 
 cd /root
 
-# intalling s3cmd
+echo
+echo " -- s3cmd -- "
+echo
 yum install git -y
 git clone https://github.com/s3tools/s3cmd.git
 cd s3cmd/
 python setup.py install
 cd /root
 
-# empty config, s3cmd will use IAM role
+echo
+echo " -- Creating empty s3cmd config, it will use IAM role -- "
 echo [default] > /root/.s3cfg
-# getting credentials
+echo " -- Getting credentials -- "
+echo
 s3cmd get s3://private.snapshots.statika.ohnosequences.com/credentials/AwsCredentials.properties
 
-# sbt
+echo
+echo " -- sbt -- "
+echo
 curl http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.3/sbt.rpm > sbt.rpm
 yum install sbt.rpm -y 
 
-# conscript
+echo
+echo " -- conscript -- "
+echo
 curl https://raw.github.com/n8han/conscript/master/setup.sh | sh
 cs --setup
 
-# giter8
+echo
+echo " -- giter8 -- "
+echo
 cs n8han/giter8
 cp /root/bin/g8 /bin/g8
 chmod a+x /bin/g8
 
-# running g8
+echo
+echo " -- running g8 -- "
+echo
 g8 ohnosequences/statika-bundle.g8 -b feature/bundle-tester '--name=BundleTester' '--ami=Ami44939930_2013_03' '--class_name=${b.name}' '--bundle_version=${b.version}' '--artifact_name=${artifact}' '--credentials=/root/AwsCredentials.properties'
 cd bundletester
-sbt run &> /root/sbt-run.out
+
+echo
+echo " -- sbt run -- "
+echo
+sbt run
 """
 }
 
 object Ami44939930App extends App { installWithDeps(Ami44939930_2013_03) }
-
-object Ami44939930Tester extends Bundle(
-    name = "Ami44939930Tester"
-  , version = Ami44939930_2013_03.version
-  , dependencies = Ami44939930_2013_03 :: HNil)
--- src/test/scala/Ami44939930Test.scala
+++ src/test/scala/Ami44939930Test.scala
@@ -4,41 +4,45 @@ import org.scalatest.FunSuite
 import shapeless._
 import ohnosequences.statika.General._
 import ohnosequences.awstools.ec2._
-import java.io._
- 
+
 class ExampleSuite extends FunSuite {
 
-    val ec2 = EC2.create(new File("AwsCredentials.properties"))
+  object Ami44939930Tester extends Bundle(
+      name = Ami44939930_2013_03.name
+    , version = Ami44939930_2013_03.version
+    , dependencies = Ami44939930_2013_03 :: HNil)
 
-    val instances = {
-      val specs = InstanceSpecs(
-          instanceType = InstanceType.InstanceType("c1.medium")
-        , amiId = "ami-44939930"
-        , keyName = "statika-launcher"
-        , deviceMapping = Map()
-        , userData = Ami44939930_2013_03.userScript(Ami44939930Tester, "ami-44939930.2013.03")
-        , instanceProfileARN = Some("arn:aws:iam::857948138625:instance-profile/statika-tester")
-        )
-      ec2.runInstances(1, specs)
-    }
+  val ec2 = EC2.create(new java.io.File("AwsCredentials.properties"))
 
-    if (instances.isEmpty) {
-      println("Couldn't access an instance for testing")
-    }
-    val instance = instances.head
+  val instances = {
+    val specs = InstanceSpecs(
+        instanceType = InstanceType.InstanceType("c1.medium")
+      , amiId = Ami44939930_2013_03.id
+      , keyName = "statika-launcher"
+      , deviceMapping = Map()
+      , userData = Ami44939930_2013_03.userScript(Ami44939930Tester, "ami-44939930.2013.03")
+      , instanceProfileARN = Some("arn:aws:iam::857948138625:instance-profile/statika-tester")
+      )
+    ec2.runInstances(1, specs)
+  }
 
-    println("Instance ID: " + instance.getInstanceId())
+  if (instances.isEmpty) {
+    println("Couldn't access an instance for testing")
+  }
+  val instance = instances.head
 
-    print("Instance initialization...")
-    while (instance.getState() != "running") {
-      Thread sleep 1000; print(".")
-    }; println("ok!")
+  println("Instance ID: " + instance.getInstanceId())
 
-    val addr = instance.getPublicDNS().get
-    println("Instance address: " + addr)
-    
-    print("Instance status ckecks...")
-    while (instance.getStatus() != Some(InstanceStatus("ok","ok"))) {
-      Thread sleep 1000; print(".")
-    }; println("ok!")
+  print("Instance initialization...")
+  while (instance.getState() != "running") {
+    Thread sleep 1000; print(".")
+  }; println("ok!")
+
+  val addr = instance.getPublicDNS().get
+  println("Instance address: " + addr)
+  
+  print("Instance status ckecks...")
+  while (instance.getStatus() != Some(InstanceStatus("ok","ok"))) {
+    Thread sleep 1000; print(".")
+  }; println("ok!")
 }
